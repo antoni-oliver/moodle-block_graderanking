3a2f30c4a546ff0e7c1ee5319ecdf56de55362d1
Antoni Oliver <antoni.oliver@uib.cat>
2024-07-04 09:41:56 +0200

    Added strings to language files.

--- lang/ca/block_graderanking.php
+++ lang/ca/block_graderanking.php
@@ -1,4 +1,5 @@
 <?php
+$string['title'] = 'Classificació de qualificació';
 $string['pluginname'] = 'Classificació de qualificació';
 $string['graderanking'] = 'Classificació de qualificació';
 $string['graderanking:addinstance'] = 'Afegeix un nou bloc de classificació de qualificació';
@@ -11,3 +12,5 @@ $string['grade'] = 'Qualificació';
 $string['tableheight'] = 'Altura de la taula (px)';
 $string['alert'] = 'Alerta';
 $string['alert_content'] = "Aquest bloc mostra una classificació d'una categoria de qualificacions.<br>Tot i que la idea és incentivar els alumnes a realitzar les tasques, si aquestes són part de la qualificació final de l'assignatura, potser l'efecte és el contrari i, a més, es faria pública una llista de (part de) les notes de l'assignatura, que potser no és desitjable.<br>S'aconsella aquest bloc per activitats opcionals, de gamificació o coses semblants.<br>Perquè això funcioni correctament, haureu de crear una categoria de qualificacions a l'apartat de qualificacions, de manera que inclogui les activitats que voleu considerar.";
+$string['missing_category_id'] = 'La categoria amb id `{$a}` no existeix.';
+$string['category_not_set_up'] = 'S\'ha de configurar la categoria.';
--- lang/en/block_graderanking.php
+++ lang/en/block_graderanking.php
@@ -1,4 +1,5 @@
 <?php
+$string['title'] = 'Grade ranking';
 $string['pluginname'] = 'Grade ranking';
 $string['graderanking'] = 'Grade ranking';
 $string['graderanking:addinstance'] = 'Add a new grade ranking block';
@@ -11,3 +12,5 @@ $string['grade'] = 'Grade';
 $string['tableheight'] = 'Table height (px)';
 $string['alert'] = 'Alert';
 $string['alert_content'] = "This block shows a ranking of a grade category.<br>Although the idea is to promote the students to complete their tasks, if these are part of the final grade of the subject, the effect may be the opposite and, furthermore, a list of (part of) the grades of the subject would be made public, which may not be desirable.<br>It is advisable to use this block for tasks that are optional, part of a gamification program or other things alike.<br>To make this work correctly, you will have to create a grade category in the gradebook, so it incldes the activities you want to consider.";
+$string['missing_category_id'] = 'The category id `{$a}` does not exist.';
+$string['category_not_set_up'] = 'The category needs to be set up.';
