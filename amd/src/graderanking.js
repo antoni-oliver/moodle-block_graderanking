801282e4abc898b29345408bf95305d4e5149ee4
Antoni Oliver <antoni.oliver@uib.cat>
2024-07-11 13:43:06 +0200

    Updated javascript with grunt

--- amd/src/graderanking.js
+++ amd/src/graderanking.js
@@ -1,11 +1,11 @@
 export const init = () => {
-        document.querySelectorAll('.graderanking_container').forEach(div => {
-            // We find the row with the "me" class.
-            const row = div.querySelector('tr.me');
-            if (!row) {
-                return;
-            }
-            // If found, we center that on the container.
-            div.scrollTo({top: row.offsetTop - div.clientHeight / 2 + row.clientHeight / 2});
-        });
+    document.querySelectorAll('.graderanking_container').forEach(div => {
+        // We find the row with the "me" class.
+        const row = div.querySelector('tr.me');
+        if (!row) {
+            return;
+        }
+        // If found, we center that on the container.
+        div.scrollTo({top: row.offsetTop - div.clientHeight / 2 + row.clientHeight / 2});
+    });
 };
