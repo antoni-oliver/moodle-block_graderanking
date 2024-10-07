d51e84c11517bd23bc473bb1ba1b46782b7d4dde
Antoni Oliver <antoni.oliver@uib.cat>
2024-10-07 14:15:25 +0200

    Fixed js comment

--- amd/src/graderanking.js
+++ amd/src/graderanking.js
@@ -2,7 +2,7 @@
  * File graderanking.js.
  * Contains client javascript for block block_graderanking.
  *
- * @package    block_graderanking
+ * @module     block_graderanking
  * @copyright  2021 Antoni Oliver
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
